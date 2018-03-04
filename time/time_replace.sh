c42f4f7bdc0603a62b9bfafa613a56dee3e46ca0
joe <little_star13@163.com>
2018-03-04 11:52:56 +0800

    update bash and time

--- time/time_replace.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# replace tags $1 old tags,$2 new tags
-
-if [[ -z $1 || -z $2 ]]
-then
-    echo "Useage: $0 oldTag newTag"
-    exit 0
-fi
--- time/time_tags.sh
+++ time/time_tags.sh
@@ -1,23 +1,111 @@
 #! /bin/bash
 
-# list all the tags in time record
+# Notes:
+# 1. if argument has space char, use 'tag name'
+# 2. if function argument has space char, use "$1"
 
-tmpfile="./.tmp_tags.md"
-if [ -e $tmpfile ]
+g_file_regexp="[\d]{2}.md"
+
+printUsage(){
+    echo "This command usage:"
+    echo "    -f tag :find tag" #$1=-f tag
+    echo "    -l     :list all the tags had used" #$1=-l
+    echo "    -r oldtag newtag [inputfile]:replace tag"  #$1=-t $2=oldtag $3=newtag
+}
+
+# $1 = tag
+findTag(){
+    files=`find . | grep -P $g_file_regexp | sort`
+    for i in $files
+    do
+        echo "$i"
+        grep -n -i "$1" $i
+        echo ""
+    done
+}
+
+listTags() {
+    tmpfile="./.tmp_tags.md"
+    if [ -e $tmpfile ]
+    then
+        echo "clear temp tags file"
+        `echo "" > $tmpfile`
+    else
+        `touch $tmpfile`
+    fi
+
+    files=`find . | grep -P $g_file_regexp | sort`
+    for i in $files
+    do
+        `cat $i | cut -d "|" -f 4 | sort -u >> $tmpfile` #4 means tag filed
+    done
+
+    tags=`sort -u $tmpfile`
+    echo "$tags"
+
+    `rm $tmpfile`
+}
+
+replaceTags(){
+
+    oldTag="$1"
+    newTag="$2"
+    file="$3"
+
+    if [[ -z $oldTag || -z $newTag ]] # -z means string's length is 0
+    then
+        echo "Error:has tag is empty."
+        return 0;
+    fi
+
+
+    files=
+    if [ -z $file ]
+    then
+        files=`find . | grep -P $g_file_regexp | sort`
+    else
+        if [ -e $file ]
+        then
+            files=$file
+        fi
+    fi
+
+    for i in $files
+    do
+        `sed -i "s/$oldTag/$newTag/i" $i`
+        if [ $? -eq 0 ]
+        then
+            echo "$i $oldTag ==> $newTag Ok"
+        else
+            echo "$i $oldTag ==> $newTag Error"
+        fi
+    done
+}
+
+# begin the bash
+
+if [ -z $1 ]
 then
-    echo "clear temp tags file"
-    `echo "" > $tmpfile`
-else
-    `touch $tmpfile`
+    printUsage
+    exit 0
 fi
 
-files=`find . | grep -P "[\d]{2}.md" | sort`
-for i in $files
-do
-    `cat $i | cut -d "|" -f 4 | sort -u >> $tmpfile` #4 means tag filed
-done
+if [ $1 == "-f" ]
+then
+    findTag "$2"
+    exit 0
+fi
 
-tags=`sort -u $tmpfile`
-echo "$tags"
+if [ $1 == "-l" ]
+then
+    listTags
+    exit 0
+fi
 
-`rm $tmpfile`
+if [ $1 == "-r" ]
+then
+    replaceTags "$2" "$3" "$4"
+    exit 0
+fi
+
+printUsage
